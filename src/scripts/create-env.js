818b455b2503568bc71181db802ae0fc2ab7e8bd
Dayyan Smith <dayyan.smith@gmail.com>
2020-04-17 15:05:26 +0200

    Add create-env script

---
+++ src/scripts/create-env.js
@@ -0,0 +1,5 @@
+const fs = require('fs')
+fs.writeFileSync('./.env.production', `GATSBY_ALGOLIA_APP_ID=${process.env.GATSBY_ALGOLIA_APP_ID}\n`)
+fs.writeFileSync('./.env.production', `GATSBY_ALGOLIA_SEARCH_KEY=${process.env.GATSBY_ALGOLIA_SEARCH_KEY}\n`)
+fs.writeFileSync('./.env.production', `ALGOLIA_ADMIN_KEY=${process.env.ALGOLIA_ADMIN_KEY}\n`)
+fs.writeFileSync('./.env.production', `BSP_API_URL=${process.env.BSP_API_URL}\n`)
