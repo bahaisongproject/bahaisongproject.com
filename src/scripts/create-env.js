d6c53f6cddd231196365516c25667ff3b151f076
Dayyan Smith <dayyan.smith@gmail.com>
2020-04-17 15:13:14 +0200

    Update script

--- src/scripts/create-env.js
+++ src/scripts/create-env.js
@@ -3,3 +3,5 @@ fs.writeFileSync('./.env.production', `GATSBY_ALGOLIA_APP_ID=${process.env.GATSB
 fs.writeFileSync('./.env.production', `GATSBY_ALGOLIA_SEARCH_KEY=${process.env.GATSBY_ALGOLIA_SEARCH_KEY}\n`)
 fs.writeFileSync('./.env.production', `ALGOLIA_ADMIN_KEY=${process.env.ALGOLIA_ADMIN_KEY}\n`)
 fs.writeFileSync('./.env.production', `BSP_API_URL=${process.env.BSP_API_URL}\n`)
+
+console.log("Created .env")
