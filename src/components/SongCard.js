b206a5da627aecca02e55dac61ebdf46a9c7222c
Dayyan Smith <dayyan.smith@gmail.com>
2020-04-19 22:03:54 +0200

    Update SongCard

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby";
 const SongCard = ({song}) => (
 <Link to={song.slug}>
 <div className="bg-orange-300 ml-0 mt-2 mb-2 mr-4 p-2 w-56 h-40 rounded-lg shadow-md">
-    <div className="flex">
+    {/* <div className="flex">
     {song.performances.length > 0 && song.performances.filter(p => p.youtube_id !== null).length > 0 ?
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" className="h-4 w-4 fill-current mr-1">
                 <path d="M0 4c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm6 0v12h8V4H6zM2 5v2h2V5H2zm0 4v2h2V9H2zm0 4v2h2v-2H2zm14-8v2h2V5h-2zm0 4v2h2V9h-2zm0 4v2h2v-2h-2zM8 7l5 3-5 3V7z"/>
@@ -17,7 +17,18 @@ const SongCard = ({song}) => (
         </svg>
     : null
     }
-    </div>
+    </div> */}
+        {(() => {
+            if (song.performances.length > 0 && song.performances.filter(p => p.youtube_id !== null).length > 0) {
+                const thumbnail_url = "https://img.youtube.com/vi/" + song.performances[0].youtube_id + "/sddefault.jpg"
+                return (
+                    <div>
+                        <img className="rounded h-16 w-full object-cover" src={thumbnail_url}/>
+                    </div>
+                    );
+            }
+            else return null;
+        })()}
     <div className="flex items-center font-semibold">
         <span className="truncate">{song.title}</span>
     </div>
@@ -26,7 +37,7 @@ const SongCard = ({song}) => (
         <div key="0"><span>{i < 1 || ", "}</span><span>{contributor.contributor_name}</span></div>
     ))}
     </div>
-    <div className="flex flex-wrap">
+    <div className="flex truncate">
       {song.languages.map((language) => (
             <div className="bg-green-300 tracking-wide text-xs text-gray-900  py-1 px-3 mr-1 mt-2 rounded-full focus:outline-none" key="0">
                 <div>{language.language_code}</div>
