0f3941eb42b0cbf5bcf7b936f9b84aef93510ce3
Dayyan Smith <dayyan.smith@gmail.com>
2020-04-21 23:56:00 +0200

    Make layout wider, remove SongCard background

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ function Layout({ children }) {
     <div className="flex flex-col min-h-screen font-sans text-gray-900">
       <Header />
 
-      <main className="flex-1 w-full max-w-4xl px-4 py-4 mx-auto md:px-8 md:py-8">
+      <main className="flex-1 w-full px-4 py-4 mx-auto md:px-8 md:py-8">
         {children}
       </main>
 
