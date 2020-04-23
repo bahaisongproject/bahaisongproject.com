e896e7c265fdbdbf845ec65af809ed256f9bc707
Dayyan Smith <dayyan.smith@gmail.com>
2020-04-23 13:13:35 +0200

    Update header

--- src/components/header.js
+++ src/components/header.js
@@ -15,7 +15,7 @@ function Header() {
 
   return (
     <header className="bg-blue-800">
-      <div className="flex flex-wrap items-center justify-between max-w-4xl p-4 mx-auto md:p-8">
+      <div className="flex flex-wrap items-center justify-between p-4 mx-auto md:p-8">
         <Link to="/">
           <h1 className="flex items-center text-white no-underline">
             <svg
