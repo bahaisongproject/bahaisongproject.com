bb95b6e22612e83705c1e35323cf58c588829710
Dayyan Smith <dayyan.smith@gmail.com>
2020-05-09 01:30:01 +0200

    Try to get contact form working on Netlify

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,78 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
+const required = "This field is required";
+
 function ContactPage() {
-  const { register, handleSubmit, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
+  const [submitted, setSubmitted] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    setError,
+    errors,
+    reset,
+    formState: { isSubmitting },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      setSubmitted(true);
+      reset();
+    } catch (error) {
+      setError(
+        "submit",
+        "submitError",
+        `Oops! There seems to be an issue! ${error.message}`
+      );
+    }
+  };
+
+  const showSubmitError = (msg) => <p className="msg-error">{msg}</p>;
+
+  const showThankYou = (
+    <div className="msg-confirm">
+      <p>Awesome! Your message was sent.</p>
+      <button type="button" onClick={() => setSubmitted(false)}>
+        Send another message?
+      </button>
+    </div>
+  );
+
+  const showForm = (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      method="post"
+      id="contact"
+      name="contact"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="bot-field" />
+      <input type="hidden" name="form-name" value="contact" />
+      <label htmlFor="name">
+        <h5>Name</h5>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Your name"
+          ref={register({ required })}
+          disabled={isSubmitting}
+        />
+        {errors.name && <div className="msg-error">{errors.name.message}</div>}
+      </label>
+
+      <label htmlFor="email">
+        <h5>Email</h5>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="your@email.address"
+          ref={register({ required })}
+          disabled={isSubmitting}
+        />
+        {errors.email && (
+          <div className="msg-error">{errors.email.message}</div>
+        )}
+      </label>
+
+      <label htmlFor="question">
+        <h5>Message</h5>
+        <textarea
+          ref={register({ required })}
+          name="question"
+          id="question"
+          rows="3"
+          placeholder="Your message"
+          disabled={isSubmitting}
+        />
+        {errors.question && (
+          <div className="msg-error">{errors.question.message}</div>
+        )}
+      </label>
+
+      <div className="submit-wrapper">
+        <button type="submit" disabled={isSubmitting}>
+          Send
+        </button>
+      </div>
+    </form>
+  );
+
   return (
     <Layout className="max-w-4xl">
       <SEO keywords={[`bahai`, `song`, `music`, `chords`]} title="Contact" />
       <h1 className="text-3xl">Contact</h1>
       <div className="w-full max-w-md">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          id="contact"
-          name="contact"
-          method="post"
-          action="/thanks"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <input type="hidden" name="bot-field" />
-          <input type="hidden" name="form-name" value="contact" />
-          <label
-            className="block text-gray-700 text-sm font-bold mt-4 mb-2"
-            htmlFor="name"
-          >
-            Name
-          </label>
-          <input
-            type="text"
-            placeholder="Name"
-            name="name"
-            id="name"
-            ref={register({ required: true, maxLength: 80 })}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-          <label
-            className="block text-gray-700 text-sm font-bold mt-4 mb-2"
-            htmlFor="email"
-          >
-            Email
-          </label>
-          <input
-            type="text"
-            placeholder="Email"
-            name="email"
-            id="email"
-            ref={register({ required: true, pattern: /^\S+@\S+$/i })}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-          <label
-            className="block text-gray-700 text-sm font-bold mt-4 mb-2"
-            htmlFor="message"
-          >
-            Message
-          </label>
-          <textarea
-            name="message"
-            id="message"
-            ref={register({ required: true })}
-            className="shadow resize-none appearance-none border rounded w-full h-32 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-
-          <button
-            type="submit"
-
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-6 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          >
-            Send
-          </button>
-        </form>
+        <div className="page contact-page">
+          <div className="text-side">
+            <h2>Contact me</h2>
+            {errors && errors.submit && showSubmitError(errors.submit.message)}
+          </div>
+          <div className="form-side">{submitted ? showThankYou : showForm}</div>
+        </div>
       </div>
     </Layout>
   );
