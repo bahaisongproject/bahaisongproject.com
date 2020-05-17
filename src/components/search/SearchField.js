6d5b84e5f889e87e3d72aca07d1c0b3bbb60f24b
Dayyan Smith <dayyan.smith@gmail.com>
2020-05-17 17:35:40 +0200

    Update SearchField position

--- src/components/search/SearchField.js
+++ src/components/search/SearchField.js
@@ -1,16 +1,12 @@
 import React from "react";
 
 const SearchField = () => (
-  <div className="w-full md:max-w-md">
-    <div className="relative ">
-      <span className="">
-        <input
-          type="search"
-          className="w-full bg-purple-white shadow-lg rounded border-0 px-3 py-2 outline-none"
-          placeholder="Search songs"
-        />
-      </span>
-    </div>
+  <div className="flex justify-center mb-6">
+      <input
+        type="search"
+        className="w-full md:max-w-md shadow-lg rounded px-3 py-2 outline-none"
+        placeholder="Search songs"
+      />
   </div>
 );
 
