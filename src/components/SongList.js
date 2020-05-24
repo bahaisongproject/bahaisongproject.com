b51b5f353322e85efe5d87f7dbb6620a10b01103
Dayyan Smith <dayyan.smith@gmail.com>
2020-05-24 21:09:47 +0200

    Add SongList horizontal alignment

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,11 +2,13 @@ import React from "react";
 import SongCard from "./SongCard";
 
 const SongList = ({ songList }) => (
-  <div className="flex flex-wrap">
-    {songList.map((hit) => (
-      <SongCard song={hit} key={hit.objectID} />
-    ))}
-  </div>
+    <div className="flex justify-center">
+        <div className="flex flex-wrap max-w-5xl">
+            {songList.map((hit) => (
+            <SongCard song={hit} key={hit.objectID} />
+            ))}
+        </div>
+    </div>
 );
 
 export default SongList;
