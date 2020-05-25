74cee8134946cd47f0d3c6c84255fc5d55d2c2ad
Dayyan Smith <dayyan.smith@gmail.com>
2020-05-25 11:04:57 +0200

    Remove extra div

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,13 +2,11 @@ import React from "react";
 import SongCard from "./SongCard";
 
 const SongList = ({ songList }) => (
-    <div className="flex justify-center">
         <div className="flex flex-wrap w-full">
             {songList.map((hit) => (
             <SongCard song={hit} key={hit.objectID} />
             ))}
         </div>
-    </div>
 );
 
 export default SongList;
