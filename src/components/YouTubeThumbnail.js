e54d2530933f3e4f4b5cc0ae6f628fa0f063a43f
Dayyan Smith <dayyan.smith@gmail.com>
2020-05-25 17:44:37 +0200

    Refactor

--- src/components/YouTubeThumbnail.js
+++ src/components/YouTubeThumbnail.js
@@ -10,9 +10,7 @@ const YouTubeThumbnail = ({ song }) => (
       ) {
           const yt_performance = song.performances.filter((p) => is_youtube(p.content_url)).sort((a, b) => a.performance_prio > b.performance_prio ? 1 : -1)[0];
           const thumbnail_url = "https://img.youtube.com/vi/" + get_youtube_id(yt_performance.content_url) + "/hqdefault.jpg";
-          if (thumbnail_url) {
-            return ( <img className="absolute top-0 left-0 h-full w-full object-cover xs:rounded-sm" src={thumbnail_url} /> );
-        }
+          return ( <img className="absolute top-0 left-0 h-full w-full object-cover xs:rounded-sm" src={thumbnail_url} /> );
       }
     })()}
   </div>
