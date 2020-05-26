3b739025eddcbb461f13eca036d29de4a4739d4e
Dayyan Smith <dayyan.smith@gmail.com>
2020-05-26 14:39:36 +0200

    Refactor

--- src/components/ContributorList.js
+++ src/components/ContributorList.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const ContributorList = ({ song }) => (
+const ContributorList = ({ song, className }) => (
   <div className="flex flex-wrap">
     {song.contributors.map((contributor, i) => (
-      <div className="contributor-name text-lg" key="0">
+      <div className={"contributor-name " + className} key="0">
         {contributor.contributor_name}
       </div>
     ))}
--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "gatsby";
 import Thumbnail from "./Thumbnail";
+import LanguageList from "./LanguageList";
+import TagList from "./TagList";
+import ContributorList from "./ContributorList";
 
 const SongCard = ({ song }) => (
   <div className="flex-grow my-4 xs:mx-4 w-full xs:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6">
@@ -14,30 +17,10 @@ const SongCard = ({ song }) => (
             <div className="text-xl leading-snug">{song.title}</div>
           </div>
         </Link>
-        <div className="flex flex-wrap text-base truncate py-1">
-          {song.contributors.map((contributor, i) => (
-            <div className="contributor-name" key="0">
-              {contributor.contributor_name}
-            </div>
-          ))}
-        </div>
+        <ContributorList className="text-gray-700" song={song} />
         <div className="flex flex-wrap">
-          {song.languages.map((language) => (
-            <div
-              className="tracking-wide text-xs border text-gray-600 bg-gray-100 my-1 mr-1 px-1 rounded-sm focus:outline-none"
-              key="0"
-            >
-              <div>{language.language_name_en}</div>
-            </div>
-          ))}
-          {song.tags.map((tag) => (
-            <div
-              className="tracking-wide text-xs border text-gray-600 bg-gray-100 my-1 mr-1 px-1 rounded-sm focus:outline-none"
-              key="0"
-            >
-              <div>{tag.tag_name}</div>
-            </div>
-          ))}
+          <LanguageList song={song} />
+          <TagList song={song} />
         </div>
       </div>
     </div>
