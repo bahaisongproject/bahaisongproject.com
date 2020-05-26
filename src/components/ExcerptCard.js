130922e9147364d569cbacec4cd78a674690514c
Dayyan Smith <dayyan.smith@gmail.com>
2020-05-26 13:43:14 +0200

    Improve margins for mobile

--- src/components/ExcerptCard.js
+++ src/components/ExcerptCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ExcerptCard = ({ excerpt }) => (
   <div
-    className="border-l-4 max-w-xl border-gray-800 p-16 my-4 bg-gray-200 transparent text-gray-900 font-serif shadow-lg rounded-sm"
+    className="border-l-4 max-w-xl border-gray-800 p-8 xs:p-16 my-4 bg-gray-200 transparent text-gray-900 font-serif shadow-lg rounded-sm"
     key="0"
   >
     {(() => {
