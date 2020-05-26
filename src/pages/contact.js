1b38e2e6444b707473cefd652fc5b5bf38c092f1
Dayyan Smith <dayyan.smith@gmail.com>
2020-05-26 19:20:33 +0200

    New approach for contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,123 +1,143 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-
 import Layout from "../components/layout";
-import SEO from "../components/seo";
 
-const required = "This field is required";
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
 
-function ContactPage() {
-  const [submitted, setSubmitted] = useState(false);
-  const {
-    register,
-    handleSubmit,
-    setError,
-    errors,
-    reset,
-    formState: { isSubmitting },
-  } = useForm();
+function Contact() {
+  const [state, setState] = useState({
+    isLoading: false,
+    submitted: false,
+    "DSVGO-Check": false,
+  });
 
-  const onSubmit = async (data) => {
-    try {
-      setSubmitted(true);
-      reset();
-    } catch (error) {
-      setError(
-        "submit",
-        "submitError",
-        `Oops! There seems to be an issue! ${error.message}`
-      );
-    }
-  };
+  function handleChange(e) {
+    const target = e.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  }
 
-  const showSubmitError = (msg) => <p className="msg-error">{msg}</p>;
-
-  const showThankYou = (
-    <div className="msg-confirm">
-      <p>Awesome! Your message was sent.</p>
-      <button type="button" onClick={() => setSubmitted(false)}>
-        Send another message?
-      </button>
-    </div>
-  );
-
-  const showForm = (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      method="post"
-      name="contact"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
-      <input type="hidden" name="form-name" value="contact" />
-      <input name="bot-field" ref={register()} />
-
-      <label htmlFor="name">
-        <h5>Name</h5>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Your name"
-          ref={register({ required })}
-          disabled={isSubmitting}
-        />
-        {errors.name && <div className="msg-error">{errors.name.message}</div>}
-      </label>
-
-      <label htmlFor="email">
-        <h5>Email</h5>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="your@email.address"
-          ref={register({ required })}
-          disabled={isSubmitting}
-        />
-        {errors.email && (
-          <div className="msg-error">{errors.email.message}</div>
-        )}
-      </label>
-
-      <label htmlFor="question">
-        <h5>Message</h5>
-        <textarea
-          ref={register({ required })}
-          name="message"
-          id="message"
-          rows="3"
-          placeholder="Your message"
-          disabled={isSubmitting}
-        />
-        {errors.question && (
-          <div className="msg-error">{errors.question.message}</div>
-        )}
-      </label>
-
-      <div className="submit-wrapper">
-        <button type="submit" disabled={isSubmitting}>
-          Send
-        </button>
-      </div>
-    </form>
-  );
+  function handleSubmit(e) {
+    e.preventDefault();
+    setState((prevState) => ({ ...prevState, isLoading: true }));
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state,
+      }),
+    })
+      .then(() =>
+        setState((prevState) => ({
+          ...prevState,
+          isLoading: false,
+          submitted: true,
+        }))
+      )
+      .catch((error) => alert(error));
+  }
 
   return (
-    <Layout className="max-w-4xl mx-auto px-4 mt-6">
-      <SEO keywords={[`bahai`, `song`, `music`, `chords`]} title="Contact" />
-      <h1 className="text-3xl">Contact</h1>
-      <div className="w-full max-w-md">
-        <div className="page contact-page">
-          <div className="text-side">
-            <h2>Contact me</h2>
-            {errors && errors.submit && showSubmitError(errors.submit.message)}
-          </div>
-          <div className="form-side">{submitted ? showThankYou : showForm}</div>
-        </div>
+    <Layout>
+      <div>
+        <p>
+          Contact
+        </p>
+      </div>
+      <div role="form">
+        <form
+          name="contact"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={handleSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out:{" "}
+              <input name="bot-field" onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <span>
+              <input
+                type="text"
+                name="name"
+                size="40"
+                aria-required="true"
+                required
+                placeholder="Name"
+                onChange={handleChange}
+              />
+            </span>
+            <br />
+            <span>
+              <input
+                type="email"
+                name="E-Mail"
+                size="40"
+                className=""
+                aria-required="true"
+                required
+                placeholder="E-Mail"
+                onChange={handleChange}
+              />
+            </span>
+            <br />
+            <span>
+              <textarea
+                name="message"
+                cols="40"
+                rows="10"
+                className=""
+                aria-required="true"
+                required
+                placeholder="Message"
+                onChange={handleChange}
+              />
+            </span>
+            <br />
+            <br />
+            <span className="gdpr-check">
+              <input
+                type="checkbox"
+                name="DSVGO-Check"
+                checked={state["DSVGO-Check"]}
+                onChange={handleChange}
+                aria-required="true"
+                required
+              />
+              <span className="">
+                GDPR
+              </span>
+            </span>
+          </p>
+          <p>
+            <input
+              className={state.submitted ? "submitted" : ""}
+              type="submit"
+              value={
+                state.isLoading
+                  ? "..."
+                  : state.submitted
+                  ? "Sent"
+                  : "Sent"
+              }
+              disabled={state["DSVGO-Check"] ? false : true}
+            />
+          </p>
+        </form>
       </div>
     </Layout>
   );
 }
 
-export default ContactPage;
+export default Contact;
