b420f4c24290ee9c3ad4eb168f0f86e885ece1cf
Dayyan Smith <dayyan.smith@gmail.com>
2020-05-26 11:15:51 +0200

    Improve ExcerptCard

--- src/components/ExcerptCard.js
+++ src/components/ExcerptCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ExcerptCard = ({ excerpt }) => (
   <div
-    className="border-l-4 max-w-xl border-gray-800 px-4 p-4 my-4 bg-gray-300 transparent text-gray-900 font-serif"
+    className="border-l-4 max-w-xl border-gray-800 p-16 my-4 bg-gray-200 transparent text-gray-900 font-serif shadow-lg rounded-sm"
     key="0"
   >
     {(() => {
@@ -13,9 +13,9 @@ const ExcerptCard = ({ excerpt }) => (
         });
       return <blockquote>{excerpt_text}</blockquote>;
     })()}
-    <div className="text-sm mt-2 flex flex-wrap justify-between">
+    <div className="text-sm mt-12 flex flex-wrap justify-between">
       <div className="">{excerpt.source.source_author}</div>
-      <div>{excerpt.source.source_description}</div>
+      <div className="">{excerpt.source.source_description}</div>
     </div>
   </div>
 );
