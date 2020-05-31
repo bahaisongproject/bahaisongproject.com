782437e31d8d534066a0bc81a9d53b7a68814f53
Dayyan Smith <dayyan.smith@gmail.com>
2020-05-31 18:33:16 +0200

    Update

--- src/components/BandcampEmbedder.js
+++ src/components/BandcampEmbedder.js
@@ -1,18 +1,9 @@
 import React from "react";
 
 const BandcampEmbedder = ({ performance }) => (
-  // <iframe
-  //   src={
-  //     performance.content_url +
-  //     "transparent=true/size=small/bgcol=ffffff/linkcol=0687f5"
-  //   }
-  //   width="100%"
-  //   height="100%"
-  //   scrolling="no"
-  //   frameBorder="no"
-  //   allow="autoplay"
-  // />
-  <iframe className="border-0 w-full h-16" src="https://bandcamp.com/EmbeddedPlayer/album=1109313778/size=small/bgcol=ffffff/linkcol=0687f5/track=1768643286/transparent=true/" seamless />
+  <iframe
+  className="border-0 w-full h-16"
+  src={performance.content_url + "/size=small/bgcol=ffffff/linkcol=0687f5/transparent=true"} seamless />
 );
 
 export default BandcampEmbedder;
