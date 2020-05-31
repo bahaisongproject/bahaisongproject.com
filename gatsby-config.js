d1847fb4a558b1f22c75b8b8e04c8979e5fc2be0
Dayyan Smith <dayyan.smith@gmail.com>
2020-05-31 19:24:34 +0200

    Remove gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,15 +57,15 @@ module.exports = {
         purgeOnly: [`src/css/style.css`],
       },
     },
-    {
-      resolve: `gatsby-plugin-algolia`,
-      options: {
-        appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_ADMIN_KEY,
-        queries,
-        chunkSize: 10000, // default: 1000
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-algolia`,
+    //   options: {
+    //     appId: process.env.GATSBY_ALGOLIA_APP_ID,
+    //     apiKey: process.env.ALGOLIA_ADMIN_KEY,
+    //     queries,
+    //     chunkSize: 10000, // default: 1000
+    //   },
+    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
