e1d2601cf2027d164aab58717dcdfec6c31d931b
Dayyan Smith <dayyan.smith@gmail.com>
2020-06-05 17:44:16 +0200

    Fix Banner in Safari

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,28 +10,15 @@ function Banner({ children, title }) {
         className="block min-w-full flex flex-col items-center justify-center focus:outline-none"
         onClick={() => toggleBannerExpansion(!bannerExpanded)}
       >
-        <div className="flex">
+        <div className="flex flex-col items-center">
           <div>{title}</div>
           <svg
-            className={`${bannerExpanded ? `hidden` : ``} w-6`}
+            className={`${bannerExpanded ? `hidden` : `w-6 inline`}`}
             fill="currentColor"
             viewBox="0 0 20 20"
           >
             <path
-              fillRule="evenodd"
               d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <svg
-            className={`${bannerExpanded ? `` : `hidden`} w-6`}
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path
-              fillRule="evenodd"
-              d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
-              clipRule="evenodd"
             ></path>
           </svg>
         </div>
@@ -41,6 +28,15 @@ function Banner({ children, title }) {
         >
           {children}
         </div>
+        <svg
+            className={`${bannerExpanded ? `` : `hidden`} w-6`}
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
+            ></path>
+        </svg>
       </button>
     </div>
   );
