d0a81907eb604676999de74fe31191167cf03808
Dayyan Smith <dayyan.smith@gmail.com>
2020-06-05 11:54:44 +0200

    Refactor SongGrid

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -7,26 +7,24 @@ import ContributorList from "./ContributorList";
 import SongDescription from "./SongDescription"
 
 const SongCard = ({ song }) => (
-  <div className="flex-grow my-4 xs:mx-4 w-full xs:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6">
-    <div className="overflow-hidden">
+  <div className="overflow-hidden">
+    <Link to={song.slug}>
+      <Thumbnail song={song} />
+    </Link>
+    <div className="pl-3 pr-1 py-2">
       <Link to={song.slug}>
-        <Thumbnail song={song} />
-      </Link>
-      <div className="pl-3 pr-1 py-2">
-        <Link to={song.slug}>
-          <div className="flex items-center">
-            <div className="text-xl font-medium leading-snug">{song.title}</div>
-          </div>
-        </Link>
-        <ContributorList className="text-gray-700 leading-tight" song={song} />
-        {(() => {
-          if (song.contributors.length === 0)
-            return <SongDescription className="text-gray-700 leading-tight" song={song} />;
-        })()}
-        <div className="flex flex-wrap">
-          <LanguageList song={song} />
-          <TagList song={song} />
+        <div className="flex items-center">
+          <div className="text-xl font-medium leading-snug">{song.title}</div>
         </div>
+      </Link>
+      <ContributorList className="text-gray-700 leading-tight" song={song} />
+      {(() => {
+        if (song.contributors.length === 0)
+          return <SongDescription className="text-gray-700 leading-tight" song={song} />;
+      })()}
+      <div className="flex flex-wrap">
+        <LanguageList song={song} />
+        <TagList song={song} />
       </div>
     </div>
   </div>
--- src/components/SongGrid.js
+++ src/components/SongGrid.js
@@ -4,7 +4,9 @@ import SongCard from "./SongCard";
 const SongGrid = ({ songList }) => (
   <div className="flex flex-wrap w-full">
     {songList.map((hit) => (
-      <SongCard song={hit} key={hit.objectID} />
+      <div key={hit.objectID} className="flex-grow my-4 xs:mx-4 w-full xs:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6">
+        <SongCard song={hit} />
+      </div>
     ))}
   </div>
 );
