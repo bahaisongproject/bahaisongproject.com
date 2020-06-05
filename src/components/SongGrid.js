b09e8e7a0d179cbd981599ab69dc642ae08c2dcf
Dayyan Smith <dayyan.smith@gmail.com>
2020-06-05 12:34:32 +0200

    Add margin to SongGrid

--- src/components/SongGrid.js
+++ src/components/SongGrid.js
@@ -2,7 +2,7 @@ import React from "react";
 import SongCard from "./SongCard";
 
 const SongGrid = ({ songList }) => (
-  <div className="grid mx-2 grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+  <div className="grid mt-6 mx-2 grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
     {songList.map((hit) => (
       <div key={hit.objectID} className="px-2">
         <SongCard song={hit} />
