57fbaba04b1611408d4f50ce5682b485f36e1bdb
Dayyan Smith <dayyan.smith@gmail.com>
2020-06-05 10:30:39 +0200

    Fix margins in excerpt card

--- src/components/ExcerptCard.js
+++ src/components/ExcerptCard.js
@@ -5,7 +5,7 @@ const ExcerptCard = ({ excerpt }) => (
     className="border-l-4 max-w-xl border-gray-800 p-8 xs:p-16 my-4 bg-gray-200 transparent text-gray-900 font-serif shadow-lg rounded-sm"
     key="0"
   >
-    <div className="p-6">
+    <div className="">
       {(() => {
         let excerpt_text = excerpt.excerpt_text
           .split("  ")
@@ -15,9 +15,9 @@ const ExcerptCard = ({ excerpt }) => (
         return <blockquote>{excerpt_text}</blockquote>;
       })()}
     </div>
-    <div className="text-sm mt-12 flex flex-wrap justify-between">
-      <div className="">{excerpt.source.source_author}</div>
-      <div className="">{excerpt.source.source_description}</div>
+    <div className="text-sm mt-8 flex flex-wrap justify-between">
+      <div className="mt-2">{excerpt.source.source_author}</div>
+      <div className="mt-2">{excerpt.source.source_description}</div>
     </div>
   </div>
 );
