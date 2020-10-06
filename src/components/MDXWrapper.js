41dfc854fd9eec6a698403c9c3d2b6248a979cda
Dayyan Smith <dayyan.smith@gmail.com>
2020-10-06 16:59:26 +0200

    Refactor ProseWrapper

--- src/components/MDXWrapper.js
+++ src/components/MDXWrapper.js
@@ -2,8 +2,9 @@ import React from "react";
 import { MDXProvider } from "@mdx-js/react";
 import Song from "./SongShowcase";
 import SongCollection from "./SongShowcaseGrid";
+import Prose from '../components/ProseWrapper'
 
-const shortcodes = { Song, SongCollection };
+const shortcodes = { Song, SongCollection, Prose };
 
 export default function MDXWrapper({ children }) {
   return <MDXProvider components={shortcodes}>{children}</MDXProvider>;
