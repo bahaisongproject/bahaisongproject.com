5784ff15ce1a14b0a1b690b1686e958d0bb2bf60
Dayyan Smith <dayyan.smith@gmail.com>
2020-10-07 19:04:30 +0200

    Add gdpr cookie banner for Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -104,25 +104,37 @@ module.exports = {
         ],
       },
     },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     // The property ID; the tracking code won't be generated without it
+    //     trackingId: "UA-27019014-2",
+    //     // Defines where to place the tracking script - `true` in the head and `false` in the body
+    //     head: false,
+    //     // Setting this parameter is optional
+    //     anonymize: true,
+    //     // Setting this parameter is also optional
+    //     respectDNT: true,
+    //     // Delays sending pageview hits on route update (in milliseconds)
+    //     pageTransitionDelay: 0,
+    //     // Defers execution of google analytics script after page load
+    //     defer: false,
+    //     // Any additional optional fields
+    //     sampleRate: 100,
+    //     siteSpeedSampleRate: 10,
+    //     cookieDomain: "bahaisongproject.com",
+    //   },
+    // },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gdpr-cookies`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-27019014-2",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 100,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "bahaisongproject.com",
+        googleAnalytics: {
+          trackingId: 'UA-27019014-2',
+          // Setting this parameter is optional
+          anonymize: true
+        },
+        // Defines the environments where the tracking should be available  - default is ["production"]
+        environments: ['production']
       },
     },
     `gatsby-plugin-sitemap`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "gatsby";
 import algoliasearch from "algoliasearch";
 import { InstantSearch } from "react-instantsearch-dom";
+import CookieConsent from 'react-cookie-consent';
 import Header from "./header";
 
 const searchClient = algoliasearch(
@@ -48,6 +49,13 @@ function Layout({ children, className, siteName }) {
           </div>
         </footer>
       </div>
+      <CookieConsent
+          // disableStyles={true}
+          location="bottom"
+          buttonText="Accept"
+          cookieName="gatsby-gdpr-google-analytics">
+      This site uses cookies. You can read about why in our <Link className="underline" to="/privacy">privacy policy</Link>.
+      </CookieConsent>
     </InstantSearch>
   );
 }
