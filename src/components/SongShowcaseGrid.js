15d69877794e47fbd5480baa06c760b0a2021998
Dayyan Smith <dayyan.smith@gmail.com>
2020-10-21 16:39:59 +0200

    Add collection

--- src/components/SongShowcaseGrid.js
+++ src/components/SongShowcaseGrid.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SongShowcaseGrid = ({ children }) => (
-  <div className="grid mt-16 col-gap-3 row-gap-6 md:col-gap-4 grid-cols-1 xs:grid-cols-2 md:grid-cols-3">
+  <div className="grid mt-16 mb-16 col-gap-3 row-gap-6 md:col-gap-4 grid-cols-1 xs:grid-cols-2 md:grid-cols-3">
     {children}
   </div>
 );
