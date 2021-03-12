2a02708810b49f32cb312d5772a955c8396ab8fc
Dayyan Smith <dayyan.smith@gmail.com>
2021-03-12 12:45:23 +0100

    Add Gatsby image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -100,7 +100,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-image`,
+    },
+    {
       resolve: `gatsby-plugin-sharp`,
+    },
+    {
+      resolve: `gatsby-transformer-sharp`,
     },
     {
       resolve: `gatsby-remark-images`,
