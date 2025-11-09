f87b1ecf23513627374442757137dd55f0386172
Dayyan Smith <7551272+daysm@users.noreply.github.com>
2025-11-09 11:16:47 +0100

    use youtube nocookie url to fix embedding error (#52)
    * use youtube nocookie url to fix embedding error
    
    * set referer policy

--- src/components/embedding/YouTubeEmbedder.js
+++ src/components/embedding/YouTubeEmbedder.js
@@ -5,9 +5,11 @@ import { get_youtube_id } from "../../utils/embed"
 const YouTubeEmbedder = ({ rendition }) => (
   <ResponsiveEmbed
     src={
-      "https://www.youtube.com/embed/" + get_youtube_id(rendition.contentUrl)
+      "https://www.youtube-nocookie.com/embed/" +
+      get_youtube_id(rendition.contentUrl)
     }
     allowFullScreen
+    referrerPolicy="strict-origin-when-cross-origin"
   />
 )
 
